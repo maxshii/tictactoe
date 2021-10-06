09da0c84f149fb0f0d3469acd9ba2edebbdb957d
U-BSD\405251 <405251@SHS-10D9C063E.beaverton.k12.or.us>
2021-10-06 12:23:13 -0700

    wrote code

---
+++ game.cpp
@@ -0,0 +1,19 @@
+/*
+ *Max Shi
+ *10/6/2021
+ *This program allows two players to play tictactoe in the command line.
+ */
+#include <iostream>
+
+using namespace::std;
+
+void printGrid();
+
+int main()
+{
+  bool play = true;
+  while(play == true)
+    {
+      
+    }
+}
