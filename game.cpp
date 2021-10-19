eade45d7cacca4b55469cc2b027472bb37de345a
U-BSD\405251 <405251@SHS-10D9C063E.beaverton.k12.or.us>
2021-10-19 12:19:39 -0700

    completed getMove method

--- game.cpp
+++ game.cpp
@@ -9,35 +9,49 @@
 
 using namespace::std;
 
-void printGrid(int array[3][3]);
+void printGrid(int **array);
 
-void getMove(int** array[3][3], int player);
+void getMove(int **array, int player);
 
 int main()
 {
   bool play = true;
   int player = 1;
+  int playerOneWin = 0;
+  int playerTwoWin = 0;
   while(play == true)
     {
       int grid[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}};
-      
+      int **ptrGrid = new int*[3];
       for (int i = 0; i < 3; i++)
 	{
-	  pointToGrid[i] = new int[3];  
+	  ptrGrid[i] = new int[3];  
 	for (int j = 0; j < 3; j++)
 	  {
-	  **pointToGrid[i][j] = grid[i][j];
+	  ptrGrid[i][j] = grid[i][j];
 	  }
 	}
-      char input[3];
-      printGrid(grid);
-      cout << "Player " << player << " move: ";
-      getMove(pointToGrid[3][3], player);
-      play = false;
+      
+      while(true) {
+	printGrid(ptrGrid);
+	cout << "Player " << player << " move: " << endl;
+	getMove(ptrGrid, player);
+	//printGrid(ptrGrid);
+
+	if(player == 1)
+	  {
+	    player = 2;
+	  }
+	else
+	  {
+	    player = 1;
+	  }
+      
+      }
     }
 }
 
-void printGrid(int array[3][3])
+void printGrid(int **array)
 {
   cout << "  1 2 3" << endl;
   char row = 'a';
@@ -52,10 +66,10 @@ void printGrid(int array[3][3])
 	    case 0:
 	      cout << "  ";
 	      break;
-	    case 1:
+	    case 1: //player 1 is X
 	      cout << "X ";
 	      break;
-	    case 2:
+	    case 2: //player 2 is O
 	      cout << "O ";
 	      break;
 	    default:
@@ -66,29 +80,45 @@ void printGrid(int array[3][3])
     }
 }
 
-void getMove(int** array[3][3], int player)
+//gets an input and checks if it is a legal move
+void getMove(int **array, int player)
 {
   char input[4];
   while(true) {
-    cout << "Enter move (row,column)" << endl;
+    cout << "Enter move: row,column" << endl;
     if (cin >> input)
       {
-	if (isalpha(input[0]) && input[1] == ',' && isdigit(input[2]))
+	//checks if format is row,column
+	if (isalpha(input[0]) != true && input[1] != ',' && isdigit(input[2]) != true) 
 	  {
-	    int row = (int)input[0] - 97;
-	    int col = (int)input[2] - 49;
+	    cout << "Wrong format" << endl;
+	    continue;
+	    }
 
-	    **array[row][col] = player;
-	    break;
-	  }
-	else
+	//turns the input into variables that can be used in the array
+	int row = (int)input[0] - 97;
+        int col = (int)input[2] - 49;
+
+	//checks if the input is in the grid
+	if((0 <= row && row < 3  && 0 <= col && col < 3) != true) 
+	   {
+	     cout << "Your input is not in the grid" << endl;
+	     continue;
+	   }
+
+	//checks if inputted space is taken
+	if(array[row][col] != 0) 
 	  {
-	    cout << "Use format (row,column)" << endl;
+	    cout << "This space is already taken" << endl;
+	    continue;
 	  }
+
+	array[row][col] = player;
+        break;
       }
     else
       {
-	cout << "Use format (row,column)" << endl;
+	cout << "Use format: row,column" << endl;
 	cin.clear();
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
       }
