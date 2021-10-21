87e835f54fba24efbe1e1fa7556001eea959868d
U-BSD\405251 <405251@SHS-10D9C063E.beaverton.k12.or.us>
2021-10-21 12:13:46 -0700

    completed tictactoe

--- game.cpp
+++ game.cpp
@@ -13,15 +13,17 @@ void printGrid(int **array);
 
 void getMove(int **array, int player);
 
+int checkWin(int **array);
+
 int main()
 {
-  bool play = true;
-  int player = 1;
   int playerOneWin = 0;
   int playerTwoWin = 0;
-  while(play == true)
+  while(true)
     {
-      int grid[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}};
+      int player = 1;
+      int move = 0;
+      int grid[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}}; //player 1 is shown as X and represented by 1, player 2 is shown as O and represented as 2
       int **ptrGrid = new int*[3];
       for (int i = 0; i < 3; i++)
 	{
@@ -31,13 +33,34 @@ int main()
 	  ptrGrid[i][j] = grid[i][j];
 	  }
 	}
-      
+      printGrid(ptrGrid);
+
+      //keeps inputting moves until board is filled or someone wins
       while(true) {
-	printGrid(ptrGrid);
 	cout << "Player " << player << " move: " << endl;
 	getMove(ptrGrid, player);
-	//printGrid(ptrGrid);
-
+	move++;
+	printGrid(ptrGrid);
+	
+	if(checkWin(ptrGrid) == 1)
+	  {
+	    cout << "Player 1 wins" << endl;
+	    playerOneWin++;
+	    break;
+	  }
+	else if(checkWin(ptrGrid) == 2)
+	  {
+	    cout << "Player 2 wins" << endl;
+	    playerTwoWin++;
+	    break;
+	  }
+	else if(move == 9) //9 moves has occured meaning board is filled and a tie has occurred
+	  {
+	    cout << "Tie" << endl;
+	    break;
+	  }
+	
+	//change player
 	if(player == 1)
 	  {
 	    player = 2;
@@ -46,8 +69,9 @@ int main()
 	  {
 	    player = 1;
 	  }
-      
       }
+
+      cout << "\nTotal Wins\nPlayer 1: " << playerOneWin << "\nPlayer 2: " << playerTwoWin << "\n\n";
     }
 }
 
@@ -123,4 +147,78 @@ void getMove(int **array, int player)
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
       }
   }
+}
+
+//returns 1 for player 1 wins, 2 for player 2 wins
+int checkWin(int** array)
+{
+  //checks rows for wins by counting X's and O's in each row
+  for(int r = 0; r < 3; r++)
+    {
+      int x = 0;
+      int o = 0;
+      for(int c = 0; c < 3; c++)
+	{
+	  switch(array[r][c])
+	    {
+	    case 1: //if X
+	      x++;
+	      break;
+	    case 2: //if O
+	      o++;
+	      break;
+	    }
+		  
+	  if(x == 3) //when there are 3 in a row it means there is a win
+	    {
+	      return 1;
+	    }
+	  else if(o == 3)
+	    {
+	      return 2;
+	    }
+	}
+    }
+  
+  //checks columns for wins by counting X's and O's in each column
+  for(int c = 0; c < 3; c++)
+    {
+      int x = 0;
+      int o = 0;
+      for(int r = 0; r < 3; r++)
+	{
+	  switch(array[r][c])
+	    {
+	    case 1: //if X
+	      x++;
+	      break;
+	    case 2: //if O
+	      o++;
+	      break;
+	    }
+		  
+	  if(x == 3) //when there are 3 in a column it means there is a win
+	    {
+	      return 1;
+	    }
+	  else if(o == 3)
+	    {
+	      return 2;
+	    }
+	}
+    }
+
+  //checks diagonals for player 1 wins then player 2 wins
+  for(int p = 1; p < 3; p++)
+    {
+      if (array[0][0] == p && array[1][1] == p && array[2][2] == p)
+	{
+	  return p;
+	}
+      else if (array[2][0] == p && array[1][1] == p && array[0][2] == p)
+	{ 
+	  return p;
+	}
+    }
+  return 0;
 }
